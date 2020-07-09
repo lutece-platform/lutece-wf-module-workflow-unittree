c7c6bc111578acb5444cb1d6e982c40bebe65aa0
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-07-09 11:37:38 +0200

    WFUNITTREE-15: Create table to store ParametrableUnitSelectionConfig

--- src/sql/plugins/workflow/modules/unittree/plugin/create_db_workflow_unittree.sql
+++ src/sql/plugins/workflow/modules/unittree/plugin/create_db_workflow_unittree.sql
@@ -30,6 +30,16 @@ CREATE TABLE workflow_task_unittree_unit_selection_specific_unit_cf (
   PRIMARY KEY (id_task)
  ) ;
 
+ /*==================================================================*/
+/* Table structure for table unittree_unit_selection_parametrable_cf   */
+/*==================================================================*/
+DROP TABLE IF EXISTS workflow_task_unittree_unit_selection_parametrable_cf;
+CREATE TABLE workflow_task_unittree_unit_selection_parametrable_cf (
+  id_task INT NOT NULL,
+  parametrable_config_handler VARCHAR(255),
+  PRIMARY KEY (id_task)
+ ) ;
+ 
 /*==================================================================*/
 /* Indexes creation for module workflow_unittree */
 /*===================================================================*/
